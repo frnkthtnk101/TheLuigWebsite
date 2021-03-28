bfd50857de7adbe34802e10ccaf6021ee17e4652
frnkthtnk101 <c0demonkers@gmail.com>
2021-03-28 13:55:43 -0400

    Working on the website
    Setting up how I want the website. Added some content

---
+++ Luig/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Luig.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Anchor { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
