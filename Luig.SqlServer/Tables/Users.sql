9c0c56bcc7115aca826e01e9e80689244d122e0f
frnkthtnk101 <c0demonkers@gmail.com>
2021-09-25 14:52:00 -0400

    Got the commons up and the connection to the database

---
+++ Luig.SqlServer/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserId]       INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [UserName]     VARCHAR(MAX) NOT NULL,
+	[UserPassword] VARCHAR(MAX) NOT NULL,
+	[UserRole]     INT NOT NULL
+)
