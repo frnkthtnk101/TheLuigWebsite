2ea7f243dd648570b34b476712996e15f0a6f9eb
frnkthtnk101 <c0demonkers@gmail.com>
2021-10-10 08:39:01 -0400

    Update LIVING_SITUATION.sql
    Living situation is now a composite key instead of two primary keys.

--- Luig.SqlServer/Tables/LIVING_SITUATION.sql
+++ Luig.SqlServer/Tables/LIVING_SITUATION.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[LIVING_SITUATION]
 (
-	[living_situation_person_id] INT NOT NULL PRIMARY KEY REFERENCES PERSON(person_id),
-	[living_situation_house_address_id] INT NOT NULL PRIMARY KEY REFERENCES HOUSE_ADDRESS(house_address_id)
+	[living_situation_person_id] INT NOT NULL  REFERENCES PERSON(person_id),
+	[living_situation_house_address_id] INT NOT NULL  REFERENCES HOUSE_ADDRESS(house_address_id), 
+    CONSTRAINT [PK_LIVING_SITUATION] PRIMARY KEY ([living_situation_person_id], [living_situation_house_address_id])
 )
